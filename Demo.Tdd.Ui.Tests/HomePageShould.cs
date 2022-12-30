d1825d6f4b52c13f97ce0834f26076bd6279d2b4
Neil Docherty <neil@jabbermouth.co.uk>
2022-12-30 23:40:01 +0000

    Test 6 fails but code compiles

---
+++ Demo.Tdd.Ui.Tests/HomePageShould.cs
@@ -0,0 +1,27 @@
+﻿using Demo.Tdd.Contentful;
+using Demo.Tdd.Ui.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Tdd.Ui.Tests;
+
+public class HomePageShould : TestContext
+{
+    [Fact]
+    public void IncludeBlogListComponent()
+    {
+        // Arrange
+        Mock<IBlogPost> blogPost = new();
+        Services.AddSingleton<IBlogPost>(blogPost.Object);
+        var put = RenderComponent<Pages.Index>();
+
+        // Act
+
+        // Assert
+        Assert.True(put.HasComponent<BlogList>());
+    }
+}
